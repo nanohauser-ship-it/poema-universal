5c99addf9a2664aa4783fcde72648a5ca710276c
Jose Balo naveiro <josebalonaveiro@iMac-de-Jose.local>
2026-09-04 17:26:53 +0200

    Poema Universal: consolidar Antologia Viva y peliculas poeticas

---
+++ app/poema-universal/antologia-viva/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AnthologyCinema from "../components/anthology-cinema/AnthologyCinema";
+
+export default function AntologiaVivaPage() {
+  return React.createElement(AnthologyCinema);
+}
